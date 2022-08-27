07e8becc57c2c17d3c4767da04b27bbea1d314b8
Amir Pirmoradian <piramir77@gmail.com>
2022-08-27 19:21:48 +0430

    add basic off code model

---
+++ app/Http/Controllers/OffCodeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OffCode;
+use Illuminate\Http\Request;
+
+class OffCodeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OffCode  $offCode
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OffCode $offCode)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OffCode  $offCode
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OffCode $offCode)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OffCode  $offCode
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OffCode $offCode)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OffCode  $offCode
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OffCode $offCode)
+    {
+        //
+    }
+}
