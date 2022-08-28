514d4bb1e5ba330a3842998e2bf23fb88b141820
Amir Pirmoradian <piramir77@gmail.com>
2022-08-28 21:27:14 +0430

    add cart

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    
+
+    public function getCart(Request $request)
+    {
+        $cart = session('cart');
+        foreach($cart['products'] as $productId => $productKey){
+            $cart['products'][$productId]['data'] = Product::find($productId)->toArray();
+        }
+        $seller = User::find($cart['sellerId']);
+        return view('cart', compact('cart', 'seller'));
+    }
+
+
+    
+}
